c398ccdde335b28ce0ab21e0976fec0b001aac54
kazama20003 <kazamatower@gmail.com>
2025-12-21 00:53:17 -0500

    /auth

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,21 +1,21 @@
 export enum UserRole {
   // Roles de la aplicación (Web/Cliente)
-  CLIENT = "CLIENT", // Usuario estándar que compra tours/productos.
+  CLIENT = "CLIENT",
 
   // Roles de Administración (Panel)
-  ADMIN = "ADMIN", // Acceso total a todas las funciones (pedidos, tours, productos, otros usuarios).
-  EDITOR = "EDITOR", // Puede gestionar tours, productos y contenido (sin modificar roles o ajustes críticos).
-  SUPPORT = "SUPPORT", // Acceso para ver pedidos y atender consultas de clientes.
+  ADMIN = "ADMIN",
+  EDITOR = "EDITOR",
+  SUPPORT = "SUPPORT",
 }
 
 export interface JwtPayload {
-  _id: string
-  email: string
-  roles: UserRole[]
-  iat: number
-  exp: number
-  firstName?: string
-  lastName?: string
+  _id: string          // viene desde jwt.strategy
+  email: string        // viene desde el JWT
+  roles: UserRole[]    // roles incluidos en el token
+  iat: number          // issued-at (automático JWT)
+  exp: number          // expiration (automático JWT)
+  firstName?: string   // opcional si lo agregas en jwt.strategy
+  lastName?: string    // opcional
 }
 
 export interface LoginCredentials {
@@ -48,11 +48,10 @@ export interface LocalRegisterCredentials {
 export interface AuthResponse {
   access_token: string
   user: {
-      id: string
-      email: string
-      roles: UserRole[]
-      firstName?: string
-      lastName?: string
-    }
-
+    id: string          // igual a _id, pero en la respuesta del login
+    email: string
+    roles: UserRole[]
+    firstName?: string
+    lastName?: string
+  }
 }
